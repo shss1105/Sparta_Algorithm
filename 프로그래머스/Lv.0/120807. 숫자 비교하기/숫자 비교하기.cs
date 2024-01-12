fa1bf0fa6d2400f2c98792cede5210f03fec5c13
shss1105 <31582497+shss1105@users.noreply.github.com>
2024-01-12 09:06:35 +0900

    [level Lv.0] Title: 숫자 비교하기, Time: 0.15 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int num1, int num2) 
+    {
+        int answer = 0;
+       
+        if(num1 >=0 && num1<=10000 && num2 >=0 && num2<=10000)
+        {
+            if (num1 == num2)
+            {
+                answer = 1;
+            }
+            else if(num1 != num2)
+            {
+                answer = -1;
+            }
+        }
+    
+        return answer;
+    }
+}
