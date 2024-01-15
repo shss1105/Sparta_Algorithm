952bf5b14c3248a259689f6d8f9a4e0ac05a938e
shss1105 <31582497+shss1105@users.noreply.github.com>
2024-01-15 09:31:37 +0900

    [level Lv.0] Title: 짝수의 합, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+        
+        if(n>0 && n<= 1000)
+        {
+            for(int i = 2; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    answer += i;
+                }
+            }
+        }
+        return answer;
+    }
+}
