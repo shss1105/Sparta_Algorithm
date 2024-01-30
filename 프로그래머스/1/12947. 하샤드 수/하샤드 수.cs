96803103b45a8a572775dbc40b9fbb29bce7fe47
shss1105 <31582497+shss1105@users.noreply.github.com>
2024-01-30 10:03:11 +0900

    Update 하샤드 수.cs
    처음에 생각한 방법

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -3,19 +3,21 @@ public class Solution
     public bool solution(int x) 
     {
         bool answer = false;
-        int numX = x;
+        string str = x.ToString();
         int sum = 0;
-        
-        while(x>0)
+        int numX = x;
+            
+        for (int i = 0; i < str.Length; i++)
         {
-            sum += x%10;
-            x = x/10;
+            sum += x % 10;
+            x /= 10;
         }
-        
-        if(numX%sum == 0)
+
+        if(numX % sum == 0)
         {
             answer = true;
         }
+            
         return answer;
     }
 }
